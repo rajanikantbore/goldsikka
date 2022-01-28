e4705c383cee8d2d2c9bd1e62285aaefeb0d98c5
rajanikantbore <borerajanikant92@gmail.com>
2022-01-28 15:36:01 +0530

    footer

---
+++ Frontend/Code/js/products.js
@@ -0,0 +1,16 @@
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) =>{
+    let ContainersDimensions = item.getBoundingClientRect();
+    let containerWidth = ContainersDimensions.width;
+    
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
